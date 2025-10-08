6043ba41b7d0cf2fcb478a7d5fc3826bf1ea7cb4
dongesz <majormate12@gmail.com>
2025-10-08 12:20:33 +0200

    feat(React): React practice

---
+++ 2025.10.08/project/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './pages/components/Nav'
+
+export default function Home() {
+
+//js
+
+  return (
+    
+    <div>
+        <Nav/>
+        <h1>Hello world!</h1>
+    </div> 
+    // html
+    
+  )
+}
---
+++ 2025.10.08/project/src/pages/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+export default function Nav() {
+  return (
+    <ul>
+    <li><a href="default.asp">Home</a></li>
+    <li><a href="news.asp">News</a></li>
+    <li><a href="contact.asp">Contact</a></li>
+    <li><a href="about.asp">About</a></li>
+    </ul>
+  )
+}
