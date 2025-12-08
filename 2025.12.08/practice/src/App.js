1244d59f48746a54fcba228339bc020a4e20a4be
Dongesz <majormate12@gmail.com>
2025-12-08 22:13:40 +0100

    feat: loading cards with data, styling with bootstrap

--- 2025.12.08/practice/src/App.js
+++ 2025.12.08/practice/src/App.js
@@ -2,11 +2,31 @@ import './App.css';
 import Card from './Components/Card';
 
 function App() {
+  var books = [
+    {
+      id : 1,
+      title: "Harry Potter es az Azkabani Fogoly",
+      text: "For Harry Potter, it’s the start of another far-from-ordinary year at Hogwarts when the Knight Bus crashes through the darkness and comes to an abrupt halt in front of him.",
+      image: "https://s06.static.libri.hu/cover/c0/6/684283_4.jpg"
+    },
+    {
+      id : 2,
+      title: "Harry Potter és a bölcsek köve",
+      text: "Turning the envelope over, his hand trembling, Harry saw a purple wax seal bearing a coat of arms; a lion, an eagle, a badger and a snake surrounding a large letter 'H'.",
+      image: "https://s04.static.libri.hu/cover/b6/f/716048_4.jpg"
+    },
+    {
+      id : 3,
+      title: "Harry Potter és a Félvér Herceg",
+      text: "One summer night, when Dumbledore arrives at Privet Drive to collect Harry Potter, his wand hand is blackened and shriveled, but he will not reveal why.",
+      image: "https://s02.static.libri.hu/cover/d8/e/716068_4.jpg"
+    },
+  ]
   return (
-    <div className="App">
-      <Card />
-      <Card />
-      <Card />
+    <div className="App row justify-content-center m-3">
+      {books.map(x =>
+        <Card id = {x.id} title={x.title} text={x.text} image={x.image}/>
+      )}
     </div>
   );
 }
--- 2025.12.08/practice/src/Components/Card.jsx
+++ 2025.12.08/practice/src/Components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({image, title, text}) {
   return (
-    <div class="card" style={{width: "18rem"}}>
-        <img class="card-img-top" src="" alt="Card image cap"/>
+    <div class="card m-3" style={{width: "18rem"}}>
+        <img class="card-img-top " style={{height: "400px"}} src={image}/>
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{text}</p>
+            <a href="#" class="btn btn-success">Expand</a>
         </div>
     </div>
   )
