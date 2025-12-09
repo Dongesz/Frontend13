a12a9e1d9fc5185ea006b94cb2356ba21c85cc83
Dongesz <majormate12@gmail.com>
2025-12-09 18:23:08 +0100

    feat: style details form - practice done

--- 2025.12.08/practice/src/Components/Details.jsx
+++ 2025.12.08/practice/src/Components/Details.jsx
@@ -2,22 +2,28 @@ import React from 'react'
 
 export default function Details({setCardColor, cardColor, setScore, score}) {
   return (
-    <div>
-        <form>
-            <input 
-              type="checkbox" 
-              id="show" name="desc" 
-              value={score}
-              onChange={(e) => setScore(!score)}/>
-            <input 
-              type="color" 
-              id="favcolor" 
-              name="favcolor" 
-              value={cardColor} 
-              onChange={(e) => setCardColor(e.target.value)}>
-            </input>
+    <div className='container d-flex justify-content-center align-items-center  bg-secondary w-25'>
+        <form className='row m-4'>
+            <div className='col'>
+              <p>Show ratings:</p>
+              <input
+                type="checkbox"
+                id="show" name="desc"
+                value={score}
+                onChange={(e) => setScore(!score)}/>
+                
+            </div>
+            <div className='col'>
+              <p>Card color:</p>
+              <input
+                type="color"
+                id="favcolor"
+                name="favcolor"
+                value={cardColor}
+                onChange={(e) => setCardColor(e.target.value)}>
+              </input>
+            </div>
         </form>
     </div>
-    
   )
 }
