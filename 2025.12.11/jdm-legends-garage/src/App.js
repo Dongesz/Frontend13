02ebf00674fcb7f24d14a06318d607153be92d3c
Dongesz <majormate12@gmail.com>
2025-12-11 23:06:07 +0100

    feat: set up react-router-dom in app.js

--- 2025.12.11/jdm-legends-garage/src/App.js
+++ 2025.12.11/jdm-legends-garage/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import Legends from './Pages/Legends';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+      </div>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/legends' element={<Legends />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
