58ad4166cb175f9169462077913a2d0d712dbbec
Dongesz <majormate12@gmail.com>
2025-12-11 23:13:10 +0100

    feat: implement navbar component with working routing

--- 2025.12.11/jdm-legends-garage/src/Components/Navbar.jsx
+++ 2025.12.11/jdm-legends-garage/src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav class="navbar navbar-expand-lg  bg-primary ">
+        <div class="container-fluid ">
+            <Link className='text-dark' to={'/'}>Home</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse m-3" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <NavLink className='text-dark' to={'/legends'}>Legends</NavLink>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
   )
 }
