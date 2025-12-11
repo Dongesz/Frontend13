db88e62f12272e2fdceaa04910123790ee198d15
Dongesz <majormate12@gmail.com>
2025-12-11 23:27:49 +0100

    feat: create legends page, fetch data from json, map data out as cards

--- 2025.12.11/jdm-legends-garage/src/Components/Card.jsx
+++ 2025.12.11/jdm-legends-garage/src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({car}) {
   return (
-    <div class="card" style={{width: "18rem"}}>
+    <div class="card m-3" style={{width: "18rem"}}>
         <img src={car.imageUrl} class="card-img-top" alt={car.name}/>
         <div class="card-body">
             <h5 class="card-title">{car.name}</h5>
--- 2025.12.11/jdm-legends-garage/src/Pages/Legends.jsx
+++ 2025.12.11/jdm-legends-garage/src/Pages/Legends.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../Components/Card'
 
 export default function Legends() {
+    const [cars, setCar] = useState([])
 
+    useEffect(() => {
+      fetch('/cars.json')
+      .then(res => res.json())
+      .then(data => setCar(data))
+    }, [])
+    
   return (
     <div>
-        
+        <div className='row d-flex justify-content-center m-4'>
+            {
+                cars.map((car, index) => (
+                    <Card key={index} car={car}/>
+                ))
+            }
+        </div>
     </div>
   )
 }
