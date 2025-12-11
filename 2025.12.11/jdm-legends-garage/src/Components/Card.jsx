9abe7d322c61150ecc34f2cebbd417d55a145716
Dongesz <majormate12@gmail.com>
2025-12-11 23:20:40 +0100

    feat: create card component with bootstrap styling

--- 2025.12.11/jdm-legends-garage/src/Components/Card.jsx
+++ 2025.12.11/jdm-legends-garage/src/Components/Card.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({car}) {
   return (
-    <div>Card</div>
+    <div class="card" style={{width: "18rem"}}>
+        <img src={car.imageUrl} class="card-img-top" alt={car.name}/>
+        <div class="card-body">
+            <h5 class="card-title">{car.name}</h5>
+            <p class="card-text">{car.legendReason}</p>
+        </div>
+    </div>
   )
 }
